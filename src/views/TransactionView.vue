2b462e4b705c45beda53beabbf07a7476deb9c35
Roland Brilianto <rolandbrilianto@gmail.com>
2022-06-27 17:56:56 +0700

    ui transaction done

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -6,44 +6,47 @@
         <div class="row">
           <div class="d-flex bd-highlight mb-3">
             <div class="me-auto p-2 bd-highlight">
-              <h3 class="mt-5 ms-5">Transaction</h3>
+              <h3 class="mt-5 ms-5 fw-bold">Transaction</h3>
             </div>
 
             <div class="p-2 bd-highlight">
               <img class="img-fluid mt-5" src="../assets/bell_vector.svg" alt="" />
             </div>
             <div class="p-2 bd-highlight">
-              <img class="img-fluid mt-5 me-5" src="../assets/user_vector.svg" alt="" />
+              <img class="img-fluid mt-5 me-5" style="cursor: pointer" data-bs-toggle="modal" data-bs-target="#exampleModal" src="../assets/user_vector.svg" alt="" />
             </div>
           </div>
         </div>
-        <div class="row ms-5 mb-5">
-          <div class="card-custom d-flex">
-            <div class="flex-coloumn">
-              <div class="mt-3 ms-3">Last Day</div>
-              <div class="mt-2 ms-3" style="font-weight: 700">65</div>
-            </div>
+        <div class="row ms-5 mb-5 d-flex">
+          <div class="card-dropdown fw-bold">
+            <div class="ms-3">Last</div>
+
+            <select class="form-select" aria-label="Default select example">
+              <option value="1">30 Days</option>
+              <option value="2">2 Weeks</option>
+              <option value="3">1 Week</option>
+            </select>
           </div>
-          <div class="card-custom d-flex">
+
+          <div class="card-custom d-flex ms-5">
             <img class="img-fluid my-auto w-25" src="../assets/material-symbols_paid.svg" alt="" />
             <div class="flex-coloumn">
               <div class="mt-3 ms-3">Transaction</div>
-              <div class="mt-2 ms-3" style="font-weight: 700">65</div>
+              <div class="mt-2 ms-3 fw-bold">65</div>
             </div>
           </div>
         </div>
         <div class="row">
           <div class="d-flex bd-highlight mb-3">
             <div class="me-auto p-2 bd-highlight">
-              <p class="ms-5" style="font-size: 24px; font-weight: 700">Latest Payment</p>
+              <p class="ms-5" style="font-size: 24px; font-weight: 700">Recent Invoices</p>
             </div>
           </div>
         </div>
         <div class="row">
-          <table class="table ms-5" style="width: 1160px">
+          <table class="table ms-5" style="width: 90%">
             <thead>
               <tr>
-                <th scope="col"></th>
                 <th scope="col">No</th>
                 <th scope="col">Date</th>
                 <th scope="col">Client</th>
@@ -53,20 +56,55 @@
             </thead>
             <tbody>
               <tr>
-                <td>
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                  </div>
-                </td>
                 <td>PQ-1124D</td>
                 <td>08/05/22</td>
                 <td>Nama Client</td>
-                <td>Telkom</td>
-                <td>$242,55</td> 
-                <td><button class="btn btn-secondary">Detail</button></td>
+                <td>$242,55</td>
+                <td>PDAM <button class="btn btn-secondary ms-5">Detail</button></td>
               </tr>
             </tbody>
           </table>
+        </div>
+
+        <!-- modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <div class="container-fluid profile">
+                  <div class="row">
+                    <div class="top-profile">
+                      <div class="d-flex"><div class="mx-auto fw-bold" style="font-size: 32px; margin-top: 80px">Profile</div></div>
+                      <div class="d-flex flex-coloumn">
+                        <img class="mx-auto" style="margin-top: 0px" src="../assets/carbon_user-avatar-filled.svg" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="low-profile">
+                    <div class="row">
+                      <div class="text-center fw-bold" style="font-size: 24px">Kelompok 39</div>
+                    </div>
+                    <div class="row"><div class="text-center" style="font-size: 18px; font-weight: 400">Kelompok39@gmail.com</div></div>
+                    <div class="row mt-4">
+                      <div class="d-flex">
+                        <button class="btn btn-secondary mx-auto text-dark fw-bold" data-bs-dismiss="modal" @click="manageAccount()" style="height: 70px; width: 90%" type="button">
+                          <div class="row">
+                            <div class="col-10 text-start">Manage Your Account</div>
+                            <div class="col-2 text-end text-black"><i class="bi bi-chevron-right"></i></div>
+                          </div>
+                        </button>
+                      </div>
+                    </div>
+                    <div class="row mt-4 mb-5">
+                      <div class="d-flex">
+                        <button class="btn btn-secondary mx-auto text-dark fw-bold text-start" data-bs-dismiss="modal" style="height: 70px; width: 90%" type="button">Sign out</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -81,9 +119,46 @@ export default {
   data() {
     return {};
   },
+  methods: {
+    manageAccount() {
+      this.$router.push("/account");
+    },
+  },
 };
 </script>
 <style scoped>
+select {
+  border: none;
+}
+.card-dropdown {
+  width: 219px;
+  height: 20px;
+  font-size: 24px;
+}
+.card-custom {
+  width: 191px;
+  height: 95px;
+  border-radius: 32px;
+  background: #ffffff;
+
+  margin-right: 32px;
+}
+.profile {
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+}
+.modal-body {
+  margin: 0;
+  padding: 0;
+}
+.low-profile {
+  margin-top: 60px;
+}
+.top-profile {
+  height: 196px;
+  background-color: #f1f1f1;
+  width: 100% !important;
+}
 .btn-danger {
   background: #d32f2f !important;
   color: #ffffff;
@@ -93,6 +168,7 @@ export default {
   background: #fff !important;
   color: #25a559;
   border-color: #25a559 !important;
+  border-radius: 32px;
 }
 .btn-secondary:hover {
   color: #25a559 !important;
@@ -101,27 +177,5 @@ export default {
   background: #25a559 !important;
   color: #fff;
   border-color: #25a559 !important;
-}
-.bg-banner {
-  background: url(../assets/card-black.svg), #f1f1f1;
-  height: 430px;
-  border-radius: 32px;
-}
-.cell {
-  overflow: auto;
-}
-h3 {
-  font-weight: 700;
-}
-.data_cont {
-  height: 200vh;
-}
-.card-custom {
-  width: 191px;
-  height: 95px;
-  border-radius: 32px;
-  background: #ffffff;
-  border: 1px solid #25a559;
-  margin-right: 32px;
 }
 </style>
